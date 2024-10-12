953d27c425a46015cdc1db75e4ec9f4992a118a7
messimack77 <mesaymekonen77@gmail.com>
2024-10-12 16:19:13 +0300

    Given a sorted array of distinct integers and a target value, return the index if the target is found.     // If not, return the index where it would be if it were inserted in order

--- src/DataStructureAndAlgorithm.java
+++ src/DataStructureAndAlgorithm.java
@@ -79,4 +79,33 @@ public class DataStructureAndAlgorithm {
         }
         return originalNumber == reversedNumber;
     }
+
+
+
+
+    //Given a sorted array of distinct integers and a target value, return the index if the target is found.
+    // If not, return the index where it would be if it were inserted in order.
+    public static int searchInsert(int[] nums, int target){
+        for (int i = 0; i<nums.length; i++){
+            if (nums[i] == target){
+                return target;
+            } else if (nums[i] > target){
+                return i;
+            }
+        }
+        return nums.length;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
