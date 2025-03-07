bcf775acd8b343879211c68300d2ec6df7b1be2a
mesay <mmekonen@chegebeya.com>
2025-03-07 09:34:43 +0300

    Resize array comment

--- src/ResizeASizeOfAnArray.java
+++ src/ResizeASizeOfAnArray.java
@@ -4,7 +4,7 @@ public class ResizeASizeOfAnArray {
 
     // resizing an array can be done by creating a new array and
     // copying the elements from the old array to the new one.
-    public static int[] resizeAnArraySize(int[] arr, int capacity){
+    public static int[] resizeAnArraySize(int[] arr, int capacity){ // arr is the old array we have and capacity is the size of the new array we are going to create
         int[] tempArray = new int[capacity];
         for (int i = 0; i<arr.length; i++){
             tempArray[i] = arr[i];
