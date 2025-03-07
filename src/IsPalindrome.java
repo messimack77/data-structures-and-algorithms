f9f039d82aa876e871c95a01d31a8eef3caa593c
mesay <mmekonen@chegebeya.com>
2025-03-07 14:45:10 +0300

    Check if the given word is palindrome

---
+++ src/IsPalindrome.java
@@ -0,0 +1,18 @@
+public class IsPalindrome {
+
+
+
+    public static boolean isPalindrome(String word){
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right){
+            if (word.charAt(left) != word.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
