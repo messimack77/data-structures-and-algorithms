50c438adcc8a77c2f96a0f4d7b119931a49fbc0e
mesay <mmekonen@chegebeya.com>
2025-03-10 10:12:00 +0300

    Binary search

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         int[] numbers = {1,2,3,4,5,6,7,8,9,10};
-        int indexOfTheNumber = BinarySearch.binarySearch(numbers, 4);
-        System.out.println(indexOfTheNumber);
+
+        System.out.println(BinarySearch.binarySearch(numbers, 4));
     }
 }
