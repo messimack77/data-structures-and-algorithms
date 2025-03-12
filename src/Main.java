fe916a4eff0b65dec383c8bb98db4a65f5ebc7a1
mesay <mmekonen@chegebeya.com>
2025-03-12 08:45:06 +0300

    Singly linked list implementation

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
-import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
- RepresentSinglyLinkedList linkedList = new RepresentSinglyLinkedList();
-        linkedList.insertAtEnd(10);
-        linkedList.insertAtEnd(20);
-        linkedList.insertAtBeginning(5);
-        linkedList.traverse();
-        linkedList.deleteNode(10);
-        linkedList.traverse();
+              SInglyLinkedListImplementation singlyLinkedList = new SInglyLinkedListImplementation();
+              singlyLinkedList.setHead(new SInglyLinkedListImplementation.ListNode(10));
+             SInglyLinkedListImplementation.ListNode second = new SInglyLinkedListImplementation.ListNode(20);
+             SInglyLinkedListImplementation.ListNode third = new SInglyLinkedListImplementation.ListNode(30);
+             SInglyLinkedListImplementation.ListNode fourth = new SInglyLinkedListImplementation.ListNode(40);
+             SInglyLinkedListImplementation.ListNode fifth = new SInglyLinkedListImplementation.ListNode(50);
+
+             singlyLinkedList.getHead().setNext(second);
+             second.setNext(third);
+             third.setNext(fourth);
+
     }
 }
